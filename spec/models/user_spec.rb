1774ee93be071cce92f0a81b6eb264a3ae4e7462
Daniel Bucher <daniel.bucher88@gmail.com>
2015-03-05 23:26:44 -0300

    Remove presence validation for users name
    This is because we don't want to have the name in the sign up form.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,7 +6,6 @@ RSpec.describe User, type: :model do
 
     it { is_expected.to validate_presence_of(:email) }
     it { is_expected.to validate_uniqueness_of(:email) }
-    it { is_expected.to validate_presence_of(:name) }
     it { is_expected.to validate_presence_of(:username) }
     it { is_expected.to validate_uniqueness_of(:username) }
   end
