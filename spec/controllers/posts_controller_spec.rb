1410c3f5f0db1bb07c4f7eea065d3c0e973d16cb
Daniel Bucher <daniel.bucher88@gmail.com>
2015-03-07 17:14:59 -0300

    Add controller tests for posts controller

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe PostsController, type: :controller do
+  describe 'create' do
+    let(:user) { FactoryGirl.create(:user) }
 
+      context 'with proper content' do
+        let(:hoot) { Post.new(content: "Hooting!") }
+
+        before :each do
+          User.expects(:find_by_username).with(user.username).returns(user)
+          post :create, content: "Hooting!", username: user.username
+        end
+
+        it { is_expected.to respond_with(:redirect) }
+      end
+  end
 end
