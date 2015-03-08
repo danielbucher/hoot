c3d477edcb426fe0c300ba856020b6bee9c892b0
Daniel Bucher <daniel.bucher88@gmail.com>
2015-03-07 21:30:49 -0300

    Improve specs for posts controller

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -5,11 +5,15 @@ RSpec.describe PostsController, type: :controller do
     let(:user) { FactoryGirl.create(:user) }
 
       context 'with proper content' do
-        let(:hoot) { Post.new(content: "Hooting!") }
+        let(:hoot) { FactoryGirl.build(:hoot_with_author) }
 
         before :each do
+          posts = mock()
           User.expects(:find_by_username).with(user.username).returns(user)
-          post :create, content: "Hooting!", username: user.username
+          user.expects(:posts).returns(posts)
+          posts.expects(:create).with(content: hoot.content).returns(hoot)
+
+          post :create, content: hoot.content, username: user.username
         end
 
         it { is_expected.to respond_with(:redirect) }
--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -1,8 +1,17 @@
 FactoryGirl.define do
   factory :post do
-    id 1
     content "I'm goint to #moe's"
-    author
-  end
 
+    trait :with_id do
+      id 1
+    end
+
+    factory :hoot_with_id, traits: [:with_id]
+
+    trait :with_author do
+      user_id 1
+    end
+
+    factory :hoot_with_author, traits: [:with_author]
+  end
 end
