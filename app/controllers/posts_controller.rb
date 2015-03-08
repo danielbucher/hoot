956fb6e3ac264847ba64ed657561b44ac91fceff
Daniel Bucher <daniel.bucher88@gmail.com>
2015-03-08 00:05:17 -0300

    Change create post request to an ajax request

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,15 +2,25 @@ class PostsController < ApplicationController
 
   def create
     @user = User.find_by_username(params[:username])
-    @post = @user.posts.create(content: params[:content])
+    @post = Post.new(content: params[:content])
+    @post.author = @user
 
-    redirect_to controller: :users, action: :home,
-      username: params[:username]
+    respond_to do |format|
+      if @post.save
+        format.html { redirect_to controller: :users, action: :home,
+           username: params[:username] }
+        format.js   {}
+      else
+        format.html { redirect_to controller: :users, action: :home,
+           username: params[:username] }
+      end
+    end
   end
 
   private
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def post_params
-      params.require(:post).permit(:content)
-    end
+
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def post_params
+    params.require(:post).permit(:content)
+  end
 end
--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -8,12 +8,23 @@ RSpec.describe PostsController, type: :controller do
         let(:hoot) { FactoryGirl.build(:hoot_with_author) }
 
         before :each do
-          posts = mock()
           User.expects(:find_by_username).with(user.username).returns(user)
-          user.expects(:posts).returns(posts)
-          posts.expects(:create).with(content: hoot.content).returns(hoot)
+          Post.any_instance.expects(:save).returns(true)
 
           post :create, content: hoot.content, username: user.username
+        end
+
+        it { is_expected.to respond_with(:redirect) }
+      end
+
+      context 'with proper content' do
+        let(:hoot) { FactoryGirl.build(:hoot_with_author) }
+
+        before :each do
+          User.expects(:find_by_username).with(user.username).returns(user)
+          Post.any_instance.expects(:save).returns(false)
+
+          post :create, content: '', username: user.username
         end
 
         it { is_expected.to respond_with(:redirect) }
