6e4735548e4b7fe777a91d4f3805ed75af85689e
Daniel Bucher <daniel.bucher88@gmail.com>
2015-03-09 19:01:10 -0300

    Add update_or_create method to Hashtag

--- app/models/hashtag.rb
+++ app/models/hashtag.rb
@@ -3,4 +3,13 @@ class Hashtag < ActiveRecord::Base
   has_and_belongs_to_many :posts, join_table: 'posts_hashtags'
 
   validates :content, presence: true
+  validates :content, uniqueness: true
+
+  def self.update_or_create(content)
+    begin
+      Hashtag.create!(content: content)
+    rescue ActiveRecord::RecordInvalid
+      Hashtag.find_by_content(content).update!(content: content)
+    end
+  end
 end
--- spec/models/hashtag_spec.rb
+++ spec/models/hashtag_spec.rb
@@ -5,6 +5,7 @@ RSpec.describe Hashtag, type: :model do
     subject { FactoryGirl.build(:hashtag) }
 
     it { is_expected.to validate_presence_of(:content) }
+    it { is_expected.to validate_uniqueness_of(:content) }
     it { should have_and_belong_to_many(:posts) }
   end
 
@@ -17,6 +18,13 @@ RSpec.describe Hashtag, type: :model do
       post.hashtags << hashtag
 
       expect(post.hashtags).to include(hashtag)
+    end
+
+    it 'should update or create hashtags' do
+      Hashtag.update_or_create("#something")
+      Hashtag.update_or_create("#something")
+
+      expect(Hashtag.where(content: "#something").count).to eq(1)
     end
   end
 end
