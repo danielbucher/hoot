b7ebe4ab70beac1a396af743fa4fc8d84b47a7da
Daniel Bucher <daniel.bucher88@gmail.com>
2015-03-10 01:23:08 -0300

    Add hour to posts view

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -2,6 +2,6 @@ module PostsHelper
 
   def short_post_date(post_id)
     post = Post.find(post_id)
-    post.updated_at.strftime("%b %d %Y")
+    post.updated_at.strftime("%b %d %Y %H:%M")
   end
 end
